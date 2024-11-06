14ac6b3d6a0dcfb397a6c0bf4d46b3f907ca800d
João Fabio <101022448+JoaoFabioPSantos@users.noreply.github.com>
2024-11-06 15:13:43 -0300

    --feat new functions

--- AthleteRegisterProject/src/AthleteRegister.java
+++ AthleteRegisterProject/src/AthleteRegister.java
@@ -3,13 +3,33 @@ public class AthleteRegister{
     private String name;
     private char gender;
     private double height;
-    private float weight;
+    private double weight;
     
-    public void Gets(String name, char gender, double height, float weight){
+    public void Gets(String name, char gender, double height, double weight){
         this.name = name;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
+    }
+    
+    public boolean verifyPositive(double value){
+        if(value>0){
+            return true;
+        }else{
+            System.out.println("Valor inválido! Digite um valor positivo: ");
+            return false;
+        }
+        
+    }
+    
+    public boolean verifyOptionGender(char value){
+        if(value=='F' || value=='M'){
+            return true;
+        }else{
+            System.out.println("Valor inválido! Favor, digitar F ou M: ");
+            return false;
+        }
+        
     }
     
     public void Report(double averageWeight, double menPerceint, String womanHeigh){
