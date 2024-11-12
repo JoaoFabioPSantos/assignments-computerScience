3a8a8c384c8db96d19ec822d7fef2634f62f280a
João Fabio <101022448+JoaoFabioPSantos@users.noreply.github.com>
2024-11-12 16:08:41 -0300

    --feat switchFunction

--- LightBulbProject/src/LightBulb.java
+++ LightBulbProject/src/LightBulb.java
@@ -16,57 +16,83 @@ public class LightBulb{
     public void setColor(String _color){
         this._color = _color;
     }
+    public String getColor(){
+        return _color;
+    }
     
     public void setBrand(String _brand){
         this._brand = _brand;
     }
+    public String getBrand(){
+        return _brand;
+    }
     
-     public void setType(String _type){
+    public void setType(String _type){
         this._type = _type;
+    }
+    public String getType(){
+        return _type;
     }
     
     public void setModel(String _model){
         this._model = _model;
     }
+    public String getModel(){
+        return _model;
+    }
     
     public void setWatss(int _watts){
         this._watts = _watts;
+    }
+    public int getWatss(){
+        return _watts;
     }
     
     public void setGuaranteeTime(int _guaranteeTime){
         this._guaranteeTime = _guaranteeTime;
     }
+    public int getGuaranteeTime(){
+        return _guaranteeTime;
+    }
     
     public void setPrice(double _price){
         this._price = _price;
     }
+    public double getPrice(){
+        return _price;
+    }
     
     public void setStatus(String _statusBulb){
+        this._statusBulb = _statusBulb;
+        
         if(_statusBulb == "Ligada"){
             _status = true;
         }else if(_statusBulb == "Desligada"){
             _status = false;
         }
     }
+    public String getStatus(){
+        return _statusBulb;
+    }
     
     
     public void turnOn(){
-        _status = true;
         System.out.println("#Lâmpada Ligada 🔆#");
+        setStatus("Ligada");
+    }
+    public void turnOff(){
+        System.out.println("#Lâmpada Desligada 🔅#");
+        setStatus("Desligada");
     }
     
-    public void turnOff(){
-        _status = false;
-        System.out.println("#Lâmpada Desligada 🔅#");
-    }
     
     public void switchMode(){
         if(!_status){
-            setStatus("Ligada");
             System.out.println("#Lâmpada Ligada 🔆#");
-        }else{
-            setStatus("Desligada");
+            setStatus("Ligada");
+        }else if(_status){
             System.out.println("#Lâmpada Desligada 🔅#");
+            setStatus("Desligada");
         }
         
     }
